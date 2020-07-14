a92a41b1e80a93c620a173ecdd4cb564627267d4
erics1996 <erics1996@yeah.net>
2020-07-14 19:51:32 +0800

    Create the files about "form".

--- app/home/views/index.py
+++ app/home/views/index.py
@@ -7,12 +7,4 @@ def user_info_list():
     return ''
 
 
-# 上映预告
-@home.route("/animation/")
-def animation():
-    return ''
 
-
-@home.route("/search/<int:page>/")
-def search(page=None):
-    return ''
--- app/home/views/movie.py
+++ app/home/views/movie.py
@@ -5,3 +5,14 @@ from flask import render_template
 @home.route("/play/<int:id>/", methods=["GET", "POST"])
 def play(id=None):
     return ''
+
+
+# 上映预告
+@home.route("/animation/")
+def animation():
+    return ''
+
+
+@home.route("/search/<int:page>/")
+def search(page=None):
+    return ''
