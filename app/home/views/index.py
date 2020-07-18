2ad2311a4490d879eadc3911071c3eecc5ef5e2e
erics1996 <erics1996@yeah.net>
2020-07-18 09:00:46 +0800

    Add the function of lazy loading.

--- app/home/views/index.py
+++ app/home/views/index.py
@@ -2,6 +2,10 @@ from app.home import home
 from flask import render_template
 
 
-@home.route('/index.html')
+@home.route('/')
 def index():
+    return render_template('home/index.html')
+
+@home.route('/index.html')
+def index2():
     return render_template('home/index.html')
